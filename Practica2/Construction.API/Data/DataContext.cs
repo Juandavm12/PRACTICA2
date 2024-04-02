cd23d62b832041563f3c494a74207dbca786d74a
Juandav12 <JuanDavid@JUANDAV12>
2024-04-02 09:24:57 -0500

    Creada Conexion y datacontext

---
+++ Practica2/Construction.API/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Construction.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            //modelBuilder.Entity<Owner>().HasIndex(c => c.Name).IsUnique();
+        }
+
+    }
+}
